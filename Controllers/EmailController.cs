dc3cb881f27cc8fc9e096c2c035dddca5db5fafc
Md Asif Khan <asif.k@technovert.com>
2024-01-23 16:21:52 +0530

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using CustomEmailKit.Models;
+using CustomEmailKit.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailPackageConsumptionApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailservice;
+
+        public EmailController(IEmailService emailservice)
+        {
+            _emailservice = emailservice;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            if (request != null)
+            {
+                _emailservice.SendEmail(request);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
